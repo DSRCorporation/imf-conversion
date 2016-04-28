3c00d3cb8ba8d59258b44702e8d3c35aaf321897
Alexandr <Alexandr>
2016-04-28 17:47:36 +0300

    Test for metadata.xml generation added

---
+++ src/main/java/com/netflix/imfutility/dpp/MetadataXml.java
@@ -0,0 +1,135 @@
+package com.netflix.imfutility.dpp;
+
+import com.netflix.imfutility.dpp.metadata.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import java.io.File;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Alexandr on 4/28/2016.
+ */
+public class MetadataXml {
+
+    public static void GenerateEmptyXml(String path) {
+        File file = new File(path);
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(Dpp.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            //Editorial section
+            EditorialType editorial = new EditorialType();
+            editorial.setSeriesTitle("");
+            editorial.setEpisodeTitleNumber("");
+            editorial.setProgrammeTitle("");
+            editorial.setProductionNumber("");
+            editorial.setSynopsis("");
+            editorial.setOriginator("");
+            editorial.setCopyrightYear(Calendar.getInstance().get(Calendar.YEAR));
+            editorial.setOtherIdentifier("");
+            editorial.setOtherIdentifierType("");
+            editorial.setGenre("");
+            editorial.setDistributor("");
+
+            //Technical section
+            TechnicalType technical = new TechnicalType();
+            technical.setShimName(ShimNameType.UK_DPP_HD);
+            technical.setShimVersion("1.1");
+
+            //Video
+            VideoType video = new VideoType();
+            video.setPictureRatio("");
+            video.setThreeD(false);
+            video.setThreeDType(ThreeDTypeType.SIDE_BY_SIDE);
+            video.setProductPlacement(false);
+            video.setPsePass(PsePassType.NOT_TESTED);
+            video.setPseManufacturer("");
+            video.setPseVersion("");
+            video.setVideoComments("");
+
+            //Audio
+            AudioType audio = new AudioType();
+            audio.setAudioTrackLayout(AudioTrackLayoutDmAs11Type.EBU_R_48_2_A);
+            audio.setPrimaryAudioLanguage(Iso6392CodeType.ZXX);
+            audio.setSecondaryAudioLanguage(Iso6392CodeType.ZXX);
+            audio.setTertiaryAudioLanguage(Iso6392CodeType.ZXX);
+            audio.setCompliantAudioStandard(CompliantAudioStandardType.NONE);
+            audio.setAudioComments("");
+
+            //Timecodes
+            TimecodesType timecodes = new TimecodesType();
+            TimecodeType zeroTimecode = new TimecodeType();
+            zeroTimecode.setValue("00:00:00:00");
+            zeroTimecode.setRate("25 1");
+            DurationType zeroDuration = new DurationType();
+            zeroDuration.setRate("25 1");
+            zeroDuration.setValue("00:00:00:00");
+            zeroDuration.setCount(1);
+            timecodes.setLineUpStart(zeroTimecode);
+            timecodes.setIdentClockStart(zeroTimecode);
+            timecodes.setTotalNumberOfParts(1);
+            timecodes.setTotalProgrammeDuration(zeroDuration);
+
+            //Timecodes parts
+            SegmentationType segmentation = new SegmentationType();
+            SegmentType segment = new SegmentType();
+            segment.setPartNumber(1);
+            segment.setPartTotal(1);
+            segment.setSOM(zeroTimecode);
+            segment.setDuration(zeroDuration);
+            segmentation.getParts().add(segment);
+            timecodes.setParts(segmentation);
+
+            //AccessService
+            AccessServicesType accessServicesType = new AccessServicesType();
+            accessServicesType.setAudioDescriptionPresent(false);
+            accessServicesType.setAudioDescriptionType(AudioDescriptionTypeType.CONTROL_DATA_NARRATION);
+            accessServicesType.setClosedCaptionsPresent(false);
+            accessServicesType.setClosedCaptionsType(CaptionsTypeType.TRANSLATION);
+            accessServicesType.setClosedCaptionsLanguage(Iso6392CodeType.ZXX);
+            accessServicesType.setOpenCaptionsPresent(false);
+            accessServicesType.setOpenCaptionsType(CaptionsTypeType.HARD_OF_HEARING);
+            accessServicesType.setOpenCaptionsLanguage(Iso6392CodeType.ZXX);
+            accessServicesType.setSigningPresent(SigningPresentType.NO);
+            accessServicesType.setSignLanguage(SignLanguageType.BRITISH_SIGN_LANGUAGE);
+
+            //Additional Section
+            AdditionalType additional = new AdditionalType();
+            additional.setCompletionDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar()));
+            additional.setTextlessElementsExist(false);
+            additional.setProgrammeHasText(false);
+            additional.setProgrammeTextLanguage(Iso6392CodeType.ZXX);
+
+            //Contact Information
+            ContactInformationType contactInformation = new ContactInformationType();
+            contactInformation.setContactEmail("account@myemail.com");
+            contactInformation.setContactTelephoneNumber("+1 000 000 0000");
+
+            // Metadata XML empty structure
+            Dpp dpp = new Dpp();
+            dpp.setEditorial(editorial);
+            dpp.setTechnical(technical);
+            technical.setVideo(video);
+            technical.setAudio(audio);
+            technical.setTimecodes(timecodes);
+            technical.setAccessServices(accessServicesType);
+            technical.setAdditional(additional);
+            technical.setContactInformation(contactInformation);
+
+            jaxbMarshaller.marshal(dpp, file);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
