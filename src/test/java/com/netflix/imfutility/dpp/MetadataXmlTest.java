813094823599d59bc1e3b373cda03e5a2cce85ba
Alexandr <Alexandr>
2016-04-29 12:34:42 +0300

    Formatting, exception catching removing

--- src/test/java/com/netflix/imfutility/dpp/MetadataXmlTest.java
+++ src/test/java/com/netflix/imfutility/dpp/MetadataXmlTest.java
@@ -12,21 +12,17 @@ import static org.junit.Assert.*;
 public class MetadataXmlTest {
     @org.junit.Test
     public void generateEmptyXml() throws Exception {
-        try{
-            //create a temp file
-            File temp = File.createTempFile(UUID.randomUUID().toString(), ".xml");
-            assertTrue("Temporary file cannot be deleted.", temp.delete());
+        //create a temp file
+        File temp = File.createTempFile(UUID.randomUUID().toString(), ".xml");
+        assertTrue("Temporary file cannot be deleted.", temp.delete());
 
-            //try to generate Dpp metadata.xml
-            MetadataXml.GenerateEmptyXml(temp.getAbsolutePath());
+        //try to generate Dpp metadata.xml
+        MetadataXml.GenerateEmptyXml(temp.getAbsolutePath());
 
-            //check it is not empty
-            assertTrue("Generated metadata.xml is zero size.", temp.length() > 0);
+        //check it is not empty
+        assertTrue("Generated metadata.xml is zero size.", temp.length() > 0);
 
-            //remove temp file;
-            temp.delete();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+        //remove temp file;
+        temp.delete();
     }
 }
