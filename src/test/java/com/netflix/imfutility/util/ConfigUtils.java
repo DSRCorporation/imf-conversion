ceafd1de7211bcce3823025ec85018f5a5265584
alexander.sherbakov <alexander.sherbakov@dsr-company.com>
2016-05-05 14:49:55 +0300

    - improved javadoc.

--- src/test/java/com/netflix/imfutility/util/ConfigUtils.java
+++ src/test/java/com/netflix/imfutility/util/ConfigUtils.java
@@ -1,7 +1,7 @@
 package com.netflix.imfutility.util;
 
 /**
- * Created by Alexander on 5/4/2016.
+ * Test utility for config.xml.
  */
 public final class ConfigUtils {
 
--- src/test/java/com/netflix/imfutility/util/ConversionUtils.java
+++ src/test/java/com/netflix/imfutility/util/ConversionUtils.java
@@ -1,7 +1,7 @@
 package com.netflix.imfutility.util;
 
 /**
- * Created by Alexander on 5/4/2016.
+ * Test utility for conversion.xml.
  */
 public final class ConversionUtils {
 
