69de5628905ea8eca3cad2ec1746e1b8c00fc295
alexander.sherbakov <alexander.sherbakov@dsr-company.com>
2016-05-18 13:56:43 +0300

    - added missing files (XSDs, test XMLs); - support of meadiInfoCommand in conversion.xml

---
+++ src/main/java/com/netflix/imfutility/cpl/SequenceInfoBuilder.java
@@ -0,0 +1,16 @@
+package com.netflix.imfutility.cpl;
+
+import com.netflix.imfutility.conversion.templateParameter.TemplateParameterContext;
+
+/**
+ * Created by Alexander on 5/18/2016.
+ */
+public class SequenceInfoBuilder {
+
+    private SequenceMap sequenceMap;
+
+    public SequenceInfoBuilder(SequenceMap sequenceMap) {
+        this.sequenceMap = sequenceMap;
+    }
+
+}
--- src/main/java/com/netflix/imfutility/cpl/SequenceMap.java
+++ src/main/java/com/netflix/imfutility/cpl/SequenceMap.java
@@ -1,6 +1,11 @@
 package com.netflix.imfutility.cpl;
 
+import com.netflix.imfutility.cpl.uuid.SequenceUUID;
+import com.netflix.imfutility.cpl.uuid.UUID;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,14 +13,20 @@ import java.util.Map;
  */
 public class SequenceMap {
 
-    private final Map<String, SequenceInfo> sequenceMap = new HashMap<>();
+    private final Map<SequenceUUID, SequenceInfo> sequenceMap = new HashMap<>();
+    private final Map<SequenceUUID, List<UUID>> sequenceAssetMap = new HashMap<>();
 
-    public void addAsset(String id, SequenceInfo asset) {
-        sequenceMap.put(id, asset);
+    public void addSequenceAsset(SequenceUUID seqUuid, UUID assetUuid) {
+        List<UUID> assets = sequenceAssetMap.get(seqUuid);
+        if (assets == null) {
+            assets = new ArrayList<>();
+            sequenceAssetMap.put(seqUuid, assets);
+        }
+        assets.add(assetUuid);
     }
 
-    public SequenceInfo getSequenceInfo(String id) {
-        return sequenceMap.get(id);
+    public List<UUID> getAssets(SequenceUUID seqUuid) {
+        return sequenceAssetMap.get(seqUuid);
     }
 
 }
