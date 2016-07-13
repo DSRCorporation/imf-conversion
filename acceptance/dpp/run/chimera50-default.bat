aaa4943894a617461937b5a6df9df42e2cb7c611
alexander.sherbakov <alexander.sherbakov@dsr-company.com>
2016-07-13 14:15:48 +0300

    - use short versions of CPLs

--- acceptance/dpp/run/chimera50-default.bat
+++ acceptance/dpp/run/chimera50-default.bat
@@ -1,2 +1,2 @@
 :: .\dpp %1 CPL_6f548f17-48c5-452a-94ea-9bb58c6c5b5b.xml %2\Chimera50 ..\chimera50\metadata.xml
-.\dpp %1 CPL-short.xml %2\Chimera50 ..\chimera50\metadata.xml
+.\dpp %1 CPL-short.xml %2\Chimera50 ..\chimera50\metadata-short.xml
