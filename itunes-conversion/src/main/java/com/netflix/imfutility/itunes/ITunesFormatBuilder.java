bf38e13b1ed0cc7b3f6c3312a7c349100db0c5c0
konstantin.goncharov <konstantin.goncharov@dsr-company.com>
2016-07-15 13:10:32 +0300

    Package folder static set generation  -added ITunesFormatBuilder  -made creating output .itmsp directory  -added iTunes stub format to conversion.xml

---
+++ itunes-conversion/src/main/java/com/netflix/imfutility/itunes/ITunesFormatBuilder.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2016 Netflix, Inc.
+ *
+ *     This file is part of IMF Conversion Utility.
+ *
+ *     IMF Conversion Utility is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     IMF Conversion Utility is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with IMF Conversion Utility.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.netflix.imfutility.itunes;
+
+import com.netflix.imfutility.AbstractFormatBuilder;
+import com.netflix.imfutility.conversion.templateParameter.context.DynamicTemplateParameterContext;
+import com.netflix.imfutility.itunes.inputparameters.ITunesInputParameters;
+import com.netflix.imfutility.itunes.inputparameters.ITunesInputParametersValidator;
+import com.netflix.imfutility.xml.XmlParsingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.netflix.imfutility.itunes.ITunesConversionConstants.DYNAMIC_PARAM_OUTPUT_ITMSP;
+
+/**
+ * iTunes format builder (see {@link AbstractFormatBuilder}). It's used for conversion to iTunes format ('convert' iTunes mode).
+ */
+public class ITunesFormatBuilder extends AbstractFormatBuilder {
+
+    private final Logger logger = LoggerFactory.getLogger(ITunesFormatBuilder.class);
+
+    private final ITunesInputParameters iTunesInputParameters;
+
+    public ITunesFormatBuilder(ITunesInputParameters inputParameters) {
+        super(new ITunesFormat(), inputParameters);
+        this.iTunesInputParameters = inputParameters;
+    }
+
+    @Override
+    protected void doValidateCmdLineArguments() {
+        ITunesInputParametersValidator.validateCmdLineArguments(iTunesInputParameters);
+    }
+
+    @Override
+    protected void doBuildDynamicContext() {
+        DynamicTemplateParameterContext dynamicContext = contextProvider.getDynamicContext();
+        // fill output parameter to [vendor-id].itmsp
+        String itmspName = iTunesInputParameters.getCmdLineArgs().getVendorId() + ".itmsp";
+        dynamicContext.addParameter(DYNAMIC_PARAM_OUTPUT_ITMSP, itmspName, false);
+    }
+
+    @Override
+    protected void preConvert() throws IOException, XmlParsingException {
+        // 1. creating [vendor-id].itmsp output directory
+        createItmspDir();
+    }
+
+    @Override
+    protected void postConvert() throws IOException, XmlParsingException {
+
+    }
+
+    @Override
+    protected String getConversionConfiguration() {
+        return conversionProvider.getConvertConfiguration().get(0);
+    }
+
+    private void createItmspDir() {
+        DynamicTemplateParameterContext dynamicContext = contextProvider.getDynamicContext();
+        String itmspName = dynamicContext.getParameterValueAsString(DYNAMIC_PARAM_OUTPUT_ITMSP);
+
+        logger.info("Creating {} output directory...", itmspName);
+
+        File itmspDir = new File(contextProvider.getWorkingDir(), itmspName);
+        logger.info("Itmsp output directory: {}", itmspDir);
+        if (!itmspDir.mkdir()) {
+            logger.error("Couldn't create {} output directory!", itmspName);
+        }
+
+        logger.info("Created {} output directory: OK\n", itmspName);
+    }
+}
