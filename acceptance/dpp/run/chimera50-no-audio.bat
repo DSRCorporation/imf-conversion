58eeae3e4c303421eeea439227b8d116998638dd
alexander.sherbakov <alexander.sherbakov@dsr-company.com>
2016-07-21 16:22:21 +0300

    fix acceptance tests

--- acceptance/dpp/run/chimera50-no-audio.bat
+++ acceptance/dpp/run/chimera50-no-audio.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd %~dp0
-.\usage-one %* && ^
+call .\usage-one %* && ^
 .\dpp %1 ..\..\CPL\chimera50\CPL-no-audio.xml "%~2\Chimera50-no-audio" ..\chimera50\metadata-short.xml "chimera50-no-audio" %3
